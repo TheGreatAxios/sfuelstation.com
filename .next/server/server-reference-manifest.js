f729b6581bbbe1afd2f717d11d93a405fb84cd35
TheGreatAxios <sawyer@dirtroad.dev>
2025-11-28 14:04:49 -0800

    Remvoe next folder

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"v+uviDu8bruowTtBr62e42ZMgKwBZltRj9UQCltaxN8=\"\n}"
--- .next/static/chunks/645ec_viem__esm_utils_ccip_0ca5483d.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/645ec_viem__esm_d665a4f6._.js"
-],
-    source: "dynamic"
-});
